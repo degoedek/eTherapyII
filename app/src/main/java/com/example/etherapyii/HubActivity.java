e575647518c0efe1b6b92bda3adee7c21b13b8b4
Joey Wagner <joeyawagner22@gmail.com>
2024-04-18 14:37:07 -0400

    Working on Bottom Nav Menu

--- app/src/main/java/com/example/etherapyii/HubActivity.java
+++ app/src/main/java/com/example/etherapyii/HubActivity.java
@@ -20,6 +20,6 @@ public class HubActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        
+
     }
 }
