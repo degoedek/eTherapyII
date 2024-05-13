a9c40a697665a4c7d6d20eae8d40deaf91dbe52d
Joey Wagner <joeyawagner22@gmail.com>
2024-05-13 10:56:24 -0400

    Navigation Menu Work I

--- app/src/main/java/com/example/etherapyii/HubActivity.java
+++ app/src/main/java/com/example/etherapyii/HubActivity.java
@@ -1,12 +1,21 @@
 package com.example.etherapyii;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentContainer;
+import androidx.fragment.app.FragmentContainerView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import org.jetbrains.annotations.NotNull;
 
 public class HubActivity extends AppCompatActivity {
 
@@ -20,6 +29,41 @@ public class HubActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        //Fragment Navigation
+        BottomNavigationView navMenu = findViewById(R.id.nav_menu);
+        navMenu.setOnItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+            if (item.getItemId() == R.id.nav_activities) {
+                selectedFragment = new HomeFragment();
+            } else if (item.getItemId() == R.id.nav_data) {
+                selectedFragment = new PersonalDataFragment();
+            }
+            // TODO: FINISH CONVERTING SWITCH TO IF
+
+
+//            switch(item.getItemId()) {
+//                case R.id.nav_activities:
+                    //selectedFragment = new ActivitySelectionFragment();
+
+//                    break;
+//                case R.id.nav_data:
+//                    selectedFragment = new PersonalDataFragment();
+//                    break;
+//                case R.id.nav_toDoList:
+//                    selectedFragment = new ToDoListFragment();
+//                    break;
+//                default:
+//                    selectedFragment = new ActivitySelectionFragment();
+//                    break;
+//            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.hub_container,selectedFragment).commit();
+            //Selects item
+            return true;
+        });
+
+
+
 
     }
 }
