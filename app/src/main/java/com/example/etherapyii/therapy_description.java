0afc9627c6684dee529463914fa35335622789f1
Joey Wagner <joeyawagner22@gmail.com>
2024-05-14 13:47:59 -0400

    Minor fixes

--- app/src/main/java/com/example/etherapyii/therapy_description.java
+++ app/src/main/java/com/example/etherapyii/therapy_description.java
@@ -1,6 +1,8 @@
 package com.example.etherapyii;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +21,17 @@ public class therapy_description extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // Variable Declaration
+        Button back = findViewById(R.id.back);
+        Button startActivity = findViewById(R.id.start_activity);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
         });
     }
 }
